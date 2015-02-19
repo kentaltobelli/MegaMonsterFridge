a247828c72d2d32dd8adeea55ef1359c35086fbd
Kent Altobelli <kent.h.altobelli@uscga.edu>
2015-02-18 21:47:36 -0500

    First commit

---
+++ MegaMonsterFridge.ino
@@ -0,0 +1,24 @@
+// MegaMonster Peltier Fridge
+// Kent Altobelli
+
+
+// Outputs
+#define MAX_COOL_PWM 100
+#define PWM_PIN 9
+
+// Inputs
+#define FRIDGE_THERM 0
+#define PELT_THERM 1
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+  delay(1000);
+}
